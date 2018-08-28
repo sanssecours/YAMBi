1c1368c68c16251b654e41ba770e8316945fa6e8
René Schwaiger <sanssecours@me.com>
2018-08-28 10:31:07 +0200

    Driver: Read input

--- Source/driver.cpp
+++ Source/driver.cpp
@@ -1,10 +1,22 @@
 #include "driver.hpp"
 
 using std::cerr;
+using std::endl;
+using std::ios_base;
 
 Driver::Driver() {}
 Driver::~Driver() {}
 
 void Driver::error(const location &location, const string &message) {
   cerr << location << ": " << message << '\n';
+}
+
+int Driver::parse(const string &filepath) {
+  filename = filepath;
+  try {
+    ifstream{filename};
+  } catch (ios_base::failure &fail) {
+    cerr << "Unable to read file: " << fail.what() << endl;
+  }
+  return 0;
 }
--- Source/driver.hpp
+++ Source/driver.hpp
@@ -1,9 +1,12 @@
 #ifndef DRIVER_HPP
 #define DRIVER_HPP
 
-#include "parser.hpp"
+#include <fstream>
 #include <string>
 
+#include "parser.hpp"
+
+using std::ifstream;
 using std::string;
 
 using yy::location;
@@ -13,11 +16,15 @@ using yy::parser;
 parser::symbol_type yylex(Driver &driver);
 
 class Driver {
+  ifstream file;
+
 public:
   string filename;
 
   Driver();
   virtual ~Driver();
+
+  int parse(const string &filename);
 
   // Error handling
   void error(const location &location, const string &message);
