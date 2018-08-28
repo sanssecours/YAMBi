810df0dd64bcd899ead32f9505f1815f7b4dc0c1
René Schwaiger <sanssecours@me.com>
2018-08-28 12:48:11 +0200

    Main: Read filename

--- Source/yambi.cpp
+++ Source/yambi.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 
-using std::cout;
+#include "driver.hpp"
+
+using std::cerr;
 using std::endl;
 
-int main() {
-  cout << "👋" << endl;
-  return 0;
+int main(int argc, char const *argv[]) {
+
+  if (argc < 2) {
+    cerr << "Usage: " << argv[0] << " filename" << endl;
+    return EXIT_FAILURE;
+  }
+
+  string filename = argv[1];
+  Driver driver;
+
+  return driver.parse(filename);
 }
