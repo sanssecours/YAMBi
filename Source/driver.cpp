261532f324765d58cab0f89d5fd3e4ae84da5d1b
René Schwaiger <sanssecours@me.com>
2018-08-29 16:52:18 +0200

    Driver: Add Doxygen comments

--- Source/driver.cpp
+++ Source/driver.cpp
@@ -1,3 +1,5 @@
+// -- Imports ------------------------------------------------------------------
+
 #include <cerrno>
 #include <fstream>
 
@@ -9,10 +11,19 @@ using std::exception;
 
 using yy::parser;
 
-void Driver::error(const location_type &location, const string &message) {
-  cerr << location << ": " << message << '\n';
-}
+// -- Class --------------------------------------------------------------------
 
+/**
+ * This function parses the current YAML file.
+ *
+ * @param filename This parameter stores the path of the file the driver
+ *                 should parse.
+ *
+ * @retval 0 if parsing was successful
+ *         EXIT_FAILURE if the driver was unable to open the given file
+ *         1 if the given file contains a syntax error
+ *         2 if parsing was unsuccessful due to memory exhaustion
+ */
 int Driver::parse(const string &filepath) {
   filename = filepath;
 
@@ -26,4 +37,15 @@ int Driver::parse(const string &filepath) {
   parser parser{*this};
 
   return parser.parse();
+}
+
+/**
+ * This function will be called by the Bison parser to indicate an error.
+ *
+ * @param location This value specifies the location of the erroneous input.
+ * @param message This value stores the error message emitted by the Bison
+ *                parser.
+ */
+void Driver::error(const location_type &location, const string &message) {
+  cerr << location << ": " << message << '\n';
 }
--- Source/driver.hpp
+++ Source/driver.hpp
@@ -1,6 +1,8 @@
 #ifndef DRIVER_HPP
 #define DRIVER_HPP
 
+// -- Imports ------------------------------------------------------------------
+
 #include <string>
 
 #include "lexer.hpp"
@@ -10,14 +12,35 @@ using std::string;
 
 using location_type = yy::parser::location_type;
 
+// -- Class --------------------------------------------------------------------
+
 class Driver {
 public:
+  /** This variable stores the path of the YAML file the driver is parsing. */
   string filename;
+  /** This attribute stores the YAML lexer for the current file. */
   Lexer lexer;
 
+  /**
+   * This function parses the current YAML file.
+   *
+   * @param filename This parameter stores the path of the file the driver
+   *                 should parse.
+   *
+   * @retval 0 if parsing was successful
+   *         EXIT_FAILURE if the driver was unable to open the given file
+   *         1 if the given file contains a syntax error
+   *         2 if parsing was unsuccessful due to memory exhaustion
+   */
   int parse(const string &filename);
 
-  // Error handling
+  /**
+   * This function will be called by the Bison parser to indicate an error.
+   *
+   * @param location This value specifies the location of the erroneous input.
+   * @param message This value stores the error message emitted by the Bison
+   *                parser.
+   */
   void error(const location_type &location, const string &message);
 };
 
