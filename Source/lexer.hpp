2bd0f571ba67dd706d6ce2a3302d68d54b7e46c2
René Schwaiger <sanssecours@me.com>
2018-08-29 14:54:28 +0200

    Lexer: Add queue to store tokens

--- Source/lexer.hpp
+++ Source/lexer.hpp
@@ -1,8 +1,12 @@
 #ifndef LEXER_HPP
 #define LEXER_HPP
 
-#include "parser.hpp"
+#include <deque>
 
+#include "parser.hpp"
+#include "symbol.hpp"
+
+using std::deque;
 using std::ifstream;
 using std::u32string;
 
@@ -11,6 +15,11 @@ using yy::parser;
 class Lexer {
   u32string input;
   size_t position;
+
+  /** This queue stores the list of tokens produced by the lexer. */
+  // Unfortunately it is not possible to store `symbol_type` in a double ended
+  // queue, since the copy assignment operator of the type is private.
+  deque<Symbol> tokens;
 
   size_t lookahead(size_t offset);
   void consume();
---
+++ Source/symbol.hpp
@@ -0,0 +1,35 @@
+#ifndef SYMBOL_HPP
+#define SYMBOL_HPP
+
+#include "parser.hpp"
+
+using std::string;
+
+using yy::parser;
+
+using location_type = parser::location_type;
+using symbol_type = parser::symbol_type;
+using token_type = parser::token_type;
+using token = parser::token;
+
+class Symbol {
+private:
+  location_type location;
+  token_type token;
+  string value;
+
+  Symbol(token_type const &token, location_type const &location,
+         string const &value)
+      : location{location}, token{token}, value{value} {}
+
+  symbol_type get() {
+    switch (token) {
+    case token::TOKEN_END:
+      return parser::make_END(location);
+    case token::TOKEN_SCALAR:
+      return parser::make_SCALAR(value, location);
+    }
+  }
+};
+
+#endif // SYMBOL_HPP
