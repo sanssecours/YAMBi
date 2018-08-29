10dfc255270dd9f41a895b6ecbba410c23d9be9b
René Schwaiger <sanssecours@me.com>
2018-08-29 20:45:32 +0200

    Lexer: Add Doxygen comments

--- Source/lexer.cpp
+++ Source/lexer.cpp
@@ -1,3 +1,5 @@
+// -- Imports ------------------------------------------------------------------
+
 #include <codecvt>
 #include <fstream>
 #include <sstream>
@@ -15,9 +17,18 @@ using yy::parser;
 using location_type = parser::location_type;
 using token = parser::token;
 
-Lexer::Lexer() {}
-Lexer::~Lexer() {}
+// -- Class --------------------------------------------------------------------
 
+/**
+ * This function returns a token the lexer has not consumed yet.
+ *
+ * @param offset This variable specifies the index of the character
+ *               this method should retrieve as offset to the last consumed
+ *               character.
+ *
+ * @return The character which is `offset` positions away from the last
+ *         consumed character
+ */
 size_t Lexer::lookahead(size_t offset) {
   if (offset == 0 || position + offset > input.size()) {
     return 0;
@@ -25,6 +36,9 @@ size_t Lexer::lookahead(size_t offset) {
   return input[static_cast<size_t>(position + offset - 1)];
 }
 
+/**
+ * This method consumes the next character of `input`.
+ */
 void Lexer::consume() {
   if (position + 1 >= input.size()) {
     throw out_of_range("Unable to consume EOF");
@@ -33,6 +47,24 @@ void Lexer::consume() {
   position++;
 }
 
+/**
+ * @brief This method adds the token for the start of the YAML stream to
+ *        `tokens`.
+ */
+void Lexer::scanStart() {
+  tokens.push_back(Symbol(token::TOKEN_STREAM_START, location, "STREAM START"));
+}
+
+/**
+ * This constructor creates an empty lexer.
+ */
+Lexer::Lexer() {}
+
+/**
+ * This constructor initializes a lexer with the given input.
+ *
+ * @param stream This stream specifies the text which this lexer analyzes.
+ */
 Lexer::Lexer(ifstream &stream) {
   scanStart();
   stringstream stringStream;
@@ -41,6 +73,16 @@ Lexer::Lexer(ifstream &stream) {
       stringStream.str());
 }
 
+/**
+ * This destructor deletes the Lexer and its instance variables.
+ */
+Lexer::~Lexer() {}
+
+/**
+ * This method returns the next token the lexer produced from `input`.
+ *
+ * @return The next token the parser has not emitted yet
+ */
 parser::symbol_type Lexer::nextToken() {
   if (tokens.size() < 1) {
     return parser::make_END(location);
@@ -49,12 +91,4 @@ parser::symbol_type Lexer::nextToken() {
   Symbol next = tokens.front();
   tokens.pop_front();
   return next.get();
-}
-
-/**
- * @brief This method adds the token for the start of the YAML stream to
- *        `tokens`.
- */
-void Lexer::scanStart() {
-  tokens.push_back(Symbol(token::TOKEN_STREAM_START, location, "STREAM START"));
 }
--- Source/lexer.hpp
+++ Source/lexer.hpp
@@ -1,6 +1,8 @@
 #ifndef LEXER_HPP
 #define LEXER_HPP
 
+// -- Imports ------------------------------------------------------------------
+
 #include <deque>
 
 #include "parser.hpp"
@@ -13,26 +15,68 @@ using std::u32string;
 using symbol_type = yy::parser::symbol_type;
 using location_type = yy::parser::location_type;
 
+// -- Class --------------------------------------------------------------------
+
 class Lexer {
+  /** This variable stores the content of the file the lexer is currently
+      parsing. */
   u32string input;
+
+  /** This variable stores the current position inside `input`. */
   size_t position;
+
+  /** This variable stores the current line and column number in Bison’s
+      location format. */
   location_type location;
 
   /** This queue stores the list of tokens produced by the lexer. */
-  // Unfortunately it is not possible to store `symbol_type` in a double ended
-  // queue, since the copy assignment operator of the type is private.
   deque<Symbol> tokens;
 
+  /**
+   * This function returns a token the lexer has not consumed yet.
+   *
+   * @param offset This variable specifies the index of the character
+   *               this method should retrieve as offset to the last consumed
+   *               character.
+   *
+   * @return The character which is `offset` positions away from the last
+   *         consumed character
+   */
   size_t lookahead(size_t offset);
+
+  /**
+   * This method consumes the next character of `input`.
+   */
   void consume();
 
+  /**
+   * This method add a token stream start symbol to the token queue.
+   */
   void scanStart();
 
 public:
+  /**
+   * This constructor creates an empty lexer.
+   */
   Lexer();
+
+  /**
+   * This constructor initializes a lexer with the given input.
+   *
+   * @param stream This stream specifies the text which this lexer analyzes.
+   */
   Lexer(ifstream &stream);
+
+  /**
+   * This destructor deletes the Lexer and its instance variables.
+   */
   virtual ~Lexer();
 
+  /**
+   * This method returns the next token the lexer produced from `input`.
+   *
+   * @return The next token the parser has not emitted yet
+   */
   symbol_type nextToken();
 };
 
