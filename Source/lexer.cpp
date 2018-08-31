be86895f4adee91b9952270272ccfe18e15aa630
René Schwaiger <sanssecours@me.com>
2018-08-31 11:37:09 +0200

    Lexer: Add logging statements

--- Source/lexer.cpp
+++ Source/lexer.cpp
@@ -4,6 +4,11 @@
 
 #include "lexer.hpp"
 
+using spdlog::set_level;
+using spdlog::set_pattern;
+using spdlog::stderr_color_mt;
+using spdlog::level::trace;
+
 using yy::parser;
 
 using location_type = parser::location_type;
@@ -16,6 +21,7 @@ using token = parser::token;
  *        `tokens`.
  */
 void Lexer::scanStart() {
+  LOG("Scan start token")
   tokens.push_back(Symbol(token::TOKEN_STREAM_START, location, "STREAM START"));
 }
 
@@ -24,7 +30,14 @@ void Lexer::scanStart() {
  *
  * @param stream This stream specifies the text which this lexer analyzes.
  */
-Lexer::Lexer(ifstream &stream) : input{stream} { scanStart(); }
+Lexer::Lexer(ifstream &stream) : input{stream} {
+  set_pattern("[%H:%M:%S:%e] %v ");
+  set_level(trace);
+  console = stderr_color_mt("console");
+  LOG("Init lexer");
+
+  scanStart();
+}
 
 /**
  * @brief This method returns the next token the lexer produced from `input`.
--- Source/lexer.hpp
+++ Source/lexer.hpp
@@ -1,17 +1,30 @@
 #ifndef LEXER_HPP
 #define LEXER_HPP
 
+// -- Macros -------------------------------------------------------------------
+
+#define SPDLOG_TRACE_ON
+#define LOGF(fmt, ...)                                                         \
+  console->trace("{}:{}: " fmt, __FUNCTION__, __LINE__, __VA_ARGS__);
+#define LOG(text) console->trace("{}:{}: {}", __FUNCTION__, __LINE__, text);
+
 // -- Imports ------------------------------------------------------------------
 
 #include <deque>
 #include <fstream>
 
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/spdlog.h>
+
 #include "input.hpp"
 #include "parser.hpp"
 #include "symbol.hpp"
 
+using spdlog::logger;
+
 using std::deque;
 using std::ifstream;
+using std::shared_ptr;
 
 using symbol_type = yy::parser::symbol_type;
 using location_type = yy::parser::location_type;
@@ -28,6 +41,11 @@ class Lexer {
 
   /** This queue stores the list of tokens produced by the lexer. */
   deque<Symbol> tokens;
+
+  /**
+   * This variable stores the logger used by the lexer to print debug messages.
+   */
+  shared_ptr<logger> console;
 
   /**
    * This method add a token stream start symbol to the token queue.
